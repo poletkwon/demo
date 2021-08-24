a709916dadebaba24d7c45c4aceec96ad753305d
poletkwon <tkwon@polestarhc.com>
2021-08-24 11:09:15 +0900

    Initial commit

---
+++ src/main/java/com/polestar/study/demo/TaewooApplication.java
@@ -0,0 +1,13 @@
+package com.polestar.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaewooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaewooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polestar/study/demo/TaewooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polestar.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaewooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
